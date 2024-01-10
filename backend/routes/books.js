b37cc075ad14a8d11d053a2ffbe0279f5e0dd354
Shashwat_Dwivedi <84010592+1999matrix@users.noreply.github.com>
2024-01-10 18:25:28 +0530

    Update books.js
    Co-authored-by: Vaibhav bansal.learn@gmail.com

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,103 +1,95 @@
-import express from "express"
-import Book from "../models/Book.js"
-import BookCategory from "../models/BookCategory.js"
+import express from "express";
+import Book from "../models/Book.js";
+import BookCategory from "../models/BookCategory.js";
 
-const router = express.Router()
+const router = express.Router();
 
-/* Get all books in the db */
+// Get all books in the db
 router.get("/allbooks", async (req, res) => {
-    try {
-        const books = await Book.find({}).populate("transactions").sort({ _id: -1 })
-        res.status(200).json(books)
-    }
-    catch (err) {
-        return res.status(504).json(err);
-    }
-})
+  try {
+    const books = await Book.find({})
+      .populate("transactions")
+      .sort({ _id: -1 });
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-/* Get Book by book Id */
+// Get Book by book Id
 router.get("/getbook/:id", async (req, res) => {
-    try {
-        const book = await Book.findById(req.params.id).populate("transactions")
-        res.status(200).json(book)
-    }
-    catch {
-        return res.status(500).json(err)
-    }
-})
+  try {
+    const book = await Book.findById(req.params.id).populate("transactions");
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-/* Get books by category name*/
+// Get books by category name
 router.get("/", async (req, res) => {
-    const category = req.query.category
-    try {
-        const books = await BookCategory.findOne({ categoryName: category }).populate("books")
-        res.status(200).json(books)
-    }
-    catch (err) {
-        return res.status(504).json(err)
-    }
-})
+  const category = req.query.category;
+  try {
+    const books = await BookCategory.findOne({ categoryName: category }).populate("books");
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-/* Adding book */
+// Adding book
 router.post("/addbook", async (req, res) => {
-    if (req.body.isAdmin) {
-        try {
-            const newbook = await new Book({
-                bookName: req.body.bookName,
-                alternateTitle: req.body.alternateTitle,
-                author: req.body.author,
-                bookCountAvailable: req.body.bookCountAvailable,
-                language: req.body.language,
-                publisher: req.body.publisher,
-                bookStatus: req.body.bookSatus,
-                categories: req.body.categories
-            })
-            const book = await newbook.save()
-            await BookCategory.updateMany({ '_id': book.categories }, { $push: { books: book._id } });
-            res.status(200).json(book)
-        }
-        catch (err) {
-            res.status(504).json(err)
-        }
-    }
-    else {
-        return res.status(403).json("You dont have permission to delete a book!");
-    }
-})
+  if (req.body.isAdmin) {
+    try {
+      const newBook = await Book.create({
+        bookName: req.body.bookName,
+        alternateTitle: req.body.alternateTitle,
+        author: req.body.author,
+        bookCountAvailable: req.body.bookCountAvailable,
+        language: req.body.language,
+        publisher: req.body.publisher,
+        bookStatus: req.body.bookStatus,
+        categories: req.body.categories,
+      });
 
-/* Addding book */
+      await BookCategory.updateMany({ '_id': newBook.categories }, { $push: { books: newBook._id } });
+
+      res.status(200).json(newBook);
+    } catch (err) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    res.status(403).json({ error: "You don't have permission to add a book!" });
+  }
+});
+
+// Updating book
 router.put("/updatebook/:id", async (req, res) => {
-    if (req.body.isAdmin) {
-        try {
-            await Book.findByIdAndUpdate(req.params.id, {
-                $set: req.body,
-            });
-            res.status(200).json("Book details updated successfully");
-        }
-        catch (err) {
-            res.status(504).json(err);
-        }
+  if (req.body.isAdmin) {
+    try {
+      await Book.findByIdAndUpdate(req.params.id, { $set: req.body });
+      res.status(200).json("Book details updated successfully");
+    } catch (err) {
+      res.status(500).json({ error: "Internal Server Error" });
     }
-    else {
-        return res.status(403).json("You dont have permission to delete a book!");
-    }
-})
+  } else {
+    res.status(403).json({ error: "You don't have permission to update a book!" });
+  }
+});
 
-/* Remove book  */
+// Remove book
 router.delete("/removebook/:id", async (req, res) => {
-    if (req.body.isAdmin) {
-        try {
-            const _id = req.params.id
-            const book = await Book.findOne({ _id })
-            await book.remove()
-            await BookCategory.updateMany({ '_id': book.categories }, { $pull: { books: book._id } });
-            res.status(200).json("Book has been deleted");
-        } catch (err) {
-            return res.status(504).json(err);
-        }
-    } else {
-        return res.status(403).json("You dont have permission to delete a book!");
+  if (req.body.isAdmin) {
+    try {
+      const book = await Book.findByIdAndRemove(req.params.id);
+      await BookCategory.updateMany({ '_id': book.categories }, { $pull: { books: book._id } });
+      res.status(200).json("Book has been deleted");
+    } catch (err) {
+      res.status(500).json({ error: "Internal Server Error" });
     }
-})
+  } else {
+    res.status(403).json({ error: "You don't have permission to delete a book!" });
+  }
+});
 
-export default router
+export default router;
